9b051d2ac9096bbe9a2e367b2b450f8b4a4bbd52
maklad42 <maklad42+git@gmail.com>
2021-04-17 18:15:25 +0900

    Add BlogLayout layout and move the header to the layout page. Also add a link to the homepage to the Blog title. 210417

--- web/src/pages/AboutPage/AboutPage.js
+++ web/src/pages/AboutPage/AboutPage.js
@@ -3,23 +3,10 @@ import { Link, routes } from '@redwoodjs/router'
 const AboutPage = () => {
   return (
     <>
-      <header>
-        <h1>Maklad's Blog</h1>
-        <nav>
-          <ul>
-            <li>
-              <Link to={routes.about()}>About</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-      <main>
-        <p>
-          This site was created to demonstrate my mastery of redwoodJS: Look
-          upon my works and despait!
-        </p>
-        <Link to={routes.home()}>Go Home</Link>
-      </main>
+      <p>
+        This site was created to demonstrate my mastery of redwoodJS: Look upon
+        my works, mere mortals, and despair!
+      </p>
     </>
   )
 }
--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -1,21 +1,5 @@
-import { Link, routes } from '@redwoodjs/router'
-
 const HomePage = () => {
-  return (
-    <>
-      <header>
-        <h1>Maklad's Blog</h1>
-        <nav>
-          <ul>
-            <li>
-              <Link to={routes.about()}>About</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-      <main>Home</main>
-    </>
-  )
+  return <p>Home</p>
 }
 
 export default HomePage
